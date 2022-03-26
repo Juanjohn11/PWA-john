377140a6f731a01ba1d8b6a46e605de471d6298c
Juanjohn11 <elmatanaco1144@gmail.com>
2022-03-26 00:30:04 -0700

    trying to deploy

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 require("dotenv").config()
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ArtMart',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/PWA-john',
   {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
